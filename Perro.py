ba31bff4a34fe71cac75d70d0c6ecab1f5c5ecd8
AkiraArkais <rockeraitor@gmail.com>
2019-05-13 17:31:58 +0200

    Add files via upload

--- Perro.py
+++ Perro.py
@@ -1,5 +1,12 @@
-class Perro():
-    def __init__(self, name, age, weight):
+class Dog():
+    def __init__(self, n, a, w):
         self.name = n
         self.age = a
         self.weight = w
+        
+    def bark(self):
+        if self.weight >=30:
+            print("GUAU, GUAU")
+        else:
+            print("guau, guau")
+        
